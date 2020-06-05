54ab1dd79c828ef6857b342e0264f17b2add5b6e
Grace Tian <gracetian4ever@gmail.com>
2020-06-05 14:32:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(array, element)
+  array.concat(element)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  
+end
